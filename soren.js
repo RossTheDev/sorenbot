b705098cd25b6b75673a31a7863058738e8057f2
ninjastaplerdev <31748549+ninjastaplerdev@users.noreply.github.com>
2017-11-08 09:28:53 -0600

    Create soren.js

---
+++ soren.js
@@ -0,0 +1,121 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+new Discord.RichEmbed();
+const newUsers = new Discord.Collection();
+const config = require("./config.json")
+
+
+
+bot.on('ready', () => {
+  console.log('Slime is bouncing');
+});
+
+
+bot.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(config.prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+  
+
+//commands.
+
+
+
+if (command === "ping") {
+    message.channel.send("pong");
+}    
+
+if (command === "embed") {
+    message.channel.send({embed: {
+  color: 7506394,
+  description: args.join(" ")
+}});
+}  
+
+if (command === "help") {
+  message.channel.send("```Info: \n  Prefix= * \n \n Regular Commands: \n  ping = Pong \n embed = Embeds any text \n sorendev = The maker of Soren bot \n \n Admin Commands: \n kick = Kicks user \n ban = bans user  ```")
+}
+
+if (command === "sorendev") {
+    message.channel.send({embed: {
+    color: 7506394, 
+    author: {
+    name: "Bot Builders",
+    icon_url: bot.user.avatarURL
+},
+ fields: [
+      {
+        name: "Bot Dev",
+        value: " <@148953784280809474>",
+      },
+      
+   //   {
+   //     name: "other contributers",
+   //     value: "",
+       ],
+    timestamp: new Date(),
+    footer: {
+      name: "",
+      text: "",
+    }
+  }
+});
+}
+//admin 
+if(command === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Mod", "Admin"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please @ a member of the server!");
+    if(!member.kickable) 
+      return message.reply("Unable to kick, Check Roles and Permissions");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please Give A Reason");
+    
+     member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+
+if(command === "ban") {
+    
+    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Mod", "Admin"].includes(r.name)) )
+      return message.reply("Must have the role: Admin, Mod, Administrator, or Moderator");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please @ a member of the server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+    
+     member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+
+if (command === "setgame") {
+  bot.user.setGame(args.join(" "));
+}
+
+
+
+
+    
+    
+});
+
+bot.login(config.token);
